1b2fe487b8a7c7caed6b010824f68a12ce5230ef
David Nilsson <nilsson.dd+github+gravatarobscurity@gmail.com>
2011-11-22 23:29:08 +0100

    Switch RNG to MWC package and insert strictness into the step-function.

--- BlackScholes.hs
+++ BlackScholes.hs
@@ -1,20 +1,43 @@
-module BlackScholes where
+module Main where
 
-import Control.Applicative
-import Data.Random.Normal (normalsIO')
+import Data.List (foldl')
+import System.Random.MWC
 
 type Rate = Double
 type Volatility = Double
 
-step :: Rate -> Volatility -> Double -> Double -> Double -> Double
-step rate volatility stepSize prev rand = prev * inner
-	where
-		inner = foldl (+) 0 [1, first, second, third]
-		first = rate*stepSize
-		second = volatility*dB
-		third = 0.5*volatility^2*(dB^2 - stepSize)
-		dB = rand * (sqrt stepSize)
-		
+step :: (Rate, Volatility) -> Double -> Double -> Double -> Double
+step (rate, volatility) stepSize prev rand = prev * inner
+        where
+                inner = foldl' (+) 0 [1, first, second, third]
+                first = rate * stepSize
+                second = volatility * dB
+                third = 0.5 * volatility^2 * (dB^2 - stepSize)
+                dB = rand * sqrt stepSize
+
+evalValue :: Int -> (Rate, Volatility) -> (Double, Double) -> [Double] -> Double
+evalValue n param (start, time) = foldl' (step param stepSize) start
+        where stepSize = time / realToFrac n
+
 main :: IO ()
-main = take n . scanl (step 0.08 0.25 0.01) 12 <$> normalsIO' (0, 1) >>= print
-	where n = truncate $ 2.0 / 0.01
+main = do
+        let n = 1000
+        let simulations = 500
+
+        rand <- genRandom (n*simulations)
+        --print $ sum rand / realToFrac 1000000
+
+        let rands = split n simulations rand
+        let res = map (evalValue n (0.08, 0.25) (12, 2)) rands
+        print $ sum res / realToFrac simulations
+
+genRandom :: Int -> IO [Double]
+genRandom num = withSystemRandom $ \gen -> do
+        let limit = num-- / realToFrac 2
+        mapM (const $ (normal :: GenIO -> IO Double) gen) [1..limit]
+        --return concat $ map (\a -> [a, negate a]) prev
+
+split :: Int -> Int -> [Double] -> [[Double]]
+split _ 0 _ = []
+split size n rand = first : split size (n - 1) sec
+        where (first, sec) = splitAt size rand
